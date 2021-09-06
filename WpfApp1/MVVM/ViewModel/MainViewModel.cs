83ec0969323264b9bb73bda44d640b90af5fe899
Brandon <bkan0458@gmail.com>
2021-09-05 22:40:21 -0600

    Add project files.

---
+++ WpfApp1/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using WpfApp1.Core;
+
+namespace WpfApp1.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand TimerViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVm{ get; set; }
+
+        public TimerViewModel TimerVM{ get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                onPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            TimerVM = new TimerViewModel();
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            TimerViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TimerVM;
+            });
+
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //    public MainWindow()
+        //    {
+        //        InitializeComponent();
+        //    }
+        //    private void Add5(object sender, RoutedEventArgs e)
+        //    {
+        //        lstNames.Items.Add("5:00");
+        //    }
+        //    private void Add10(object sender, RoutedEventArgs e)
+        //    {
+        //        lstNames.Items.Add("10:00");
+        //    }
+        //    private void Add15(object sender, RoutedEventArgs e)
+        //    {
+        //        lstNames.Items.Add("15:00");
+        //    }
+        //    private void AddBreak(object sender, RoutedEventArgs e)
+        //    {
+        //        lstNames.Items.Add("5:00");
+        //    }
+        //}
+
+    }
+}
+
+0
